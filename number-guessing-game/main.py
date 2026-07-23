0370e1d07e18228156ba5d2d27c5988136b51170
lakshitkumawat25-dev <lakshit.kumawat25@gmail.com>
2026-07-24 00:06:24 +0530

    Implement number guessing game logic

--- number-guessing-game/main.py
+++ number-guessing-game/main.py
@@ -1,1 +1,17 @@
+import random
+secret_no = random.randint(1,100)
 
+print("guess no bw 1 to 100")
+
+while True:
+    guess_no = int(input("enter no"))
+    if guess_no==secret_no:
+        print("you guessed the no")
+        break
+    elif guess_no<secret_no:
+        print("higher")
+    elif guess_no>secret_no:
+        print("lower")
+
+print("GAME OVER")
+    
